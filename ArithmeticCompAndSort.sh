e6689dc5f49a30a23c398fba538a15952d108c4c
unknown <manishasingh2203@gmail.com>
2020-09-08 22:20:38 +0530

    UC6 done

--- ArithmeticCompAndSort.sh
+++ ArithmeticCompAndSort.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 echo This problem contains different arithmetic expressions and sorts the results
+declare -A result
 read -p "Enter the value for a" a
 read -p "Enter the value for b" b
 read -p "Enter the value for c" c
@@ -8,3 +9,7 @@ arith1=$((a+b*c))
 arith2=$((a*b+c))
 arith3=$((c+a/b))
 arith4=$((a%b+c))
+result[uc2]=$arith1
+result[uc3]=$arith2
+result[uc4]=$arith3
+result[uc5]=$arith4

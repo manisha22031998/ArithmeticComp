465b109e92b4043dfbbfd6be1dd3ba676ddc1b4a
unknown <manishasingh2203@gmail.com>
2020-09-08 21:22:15 +0530

    UC1 done

--- ArithmeticCompAndSort.sh
+++ ArithmeticCompAndSort.sh
@@ -1,2 +1,6 @@
 #! /bin/bash
+
 echo This problem contains different arithmetic expressions and sorts the results
+read -p "Enter the value for a" a
+read -p "Enter the value for b" b
+read -p "Enter the value for c" c

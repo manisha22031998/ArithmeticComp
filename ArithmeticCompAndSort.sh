9838fbf856e48c58ac4274ea3fab6a1b980121d3
unknown <manishasingh2203@gmail.com>
2020-09-08 22:25:13 +0530

    UC7 done

--- ArithmeticCompAndSort.sh
+++ ArithmeticCompAndSort.sh
@@ -2,6 +2,8 @@
 
 echo This problem contains different arithmetic expressions and sorts the results
 declare -A result
+declare -a arr
+i=0
 read -p "Enter the value for a" a
 read -p "Enter the value for b" b
 read -p "Enter the value for c" c
@@ -13,3 +15,8 @@ result[uc2]=$arith1
 result[uc3]=$arith2
 result[uc4]=$arith3
 result[uc5]=$arith4
+for value in ${result[@]}
+do
+	arr[((i++))]=$value
+done
+echo ${arr[@]}

6dd8306025f7b042e2b7fd3de279bad69125ea0f
unknown <manishasingh2203@gmail.com>
2020-09-08 21:24:27 +0530

    UC2 done

--- ArithmeticCompAndSort.sh
+++ ArithmeticCompAndSort.sh
@@ -4,3 +4,4 @@ echo This problem contains different arithmetic expressions and sorts the result
 read -p "Enter the value for a" a
 read -p "Enter the value for b" b
 read -p "Enter the value for c" c
+arith1=$((a+b*c))
